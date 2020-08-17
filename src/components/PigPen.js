06887bb68493674315046087589ab2adb0793b75
Jessica <github email address>
2020-08-17 18:23:33 +0900

    Done.

--- src/components/PigPen.js
+++ src/components/PigPen.js
@@ -1,15 +1,8 @@
-import React from "react"
-import Pig from "./Pig.js"
-import GalaxySNote7 from "./GalaxySNote7.js"
+import React from "react";
+import Pig from "./Pig.js";
+import GalaxySNote7 from "./GalaxySNote7.js";
 import exclaim from '../assets/exclaim.mp3';
-
-
-const pigs = [
-  "Sobriety",
-  "Trouble",
-  "Cherub",
-  "MasterBlaster"
-]
+import hogs from '../data/hogs.js';
 
 export default class PigPen extends React.Component {
   constructor() {
@@ -17,34 +10,35 @@ export default class PigPen extends React.Component {
     this.state = {
       environment: "docile"
     }
-    this.audio = new Audio(exclaim)
-  }
+    this.audio = new Audio(exclaim);
+  };
 
   relax = () => {
-    const newState = {environment: 'docile'}
-    this.setState(newState)
-  }
+    const resetState = {environment: "docile"}
+    this.setState(resetState)
+  };
 
   alterEnvironment = (vibe) => {
     if (vibe === "inhospitable")
       this.audio.play()
     const newState = {environment: vibe}
     this.setState(newState)
-  }
+    setTimeout(this.relax, 2000)
+  };
 
   generateSheeple = () => {
-    return pigs.map((name, idx) => (
+    return hogs.map((name, idx) => (
       <Pig key={idx} id={name} name={name} environment={this.state.environment} />
     ))
-  }
+  };
 
   render() {
     const sheeple = this.generateSheeple()
     return(
       <div id="pig-pen">
         {sheeple}
-        <GalaxySNote7 environment={null} alterEnvironment={null} />
+        <GalaxySNote7 environment={this.state.envirionment} alterEnvironment={this.alterEnvironment} />
       </div>
     )
-  }
-}
+  };
+};
